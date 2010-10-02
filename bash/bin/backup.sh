6d4b95dc589dcb574889a8d276f167b046850ead
Gareth Jones <gareth.e.jones@gmail.com>
2010-10-02 17:18:16 -0500

    fixing backup script, make clj use brew, functions for switching between cl and clojure emacs setup, adding screenrc

--- bash/bin/backup.sh
+++ bash/bin/backup.sh
@@ -1,7 +1,8 @@
 EPHALENT_IP=10.0.1.4
-rsync -avu -e "ssh -i /Users/garethjones/.ssh/id_rsa.pub" ~/Music/mp3/ admin@$EPHALENT_IP:/volume1/music/
-rsync -avu -e "ssh -i /Users/garethjones/.ssh/id_rsa.pub" ~/Documents/ admin@$EPHALENT_IP:/volume1/homes/gaz/documents/
-rsync -avu -e "ssh -i /Users/garethjones/.ssh/id_rsa.pub" ~/Library/Application\ Support/1Password/Backups/ admin@$EPHALENT_IP:/volume1/homes/gaz/backups/1password/
+rsync -avu -e "ssh -l admin" ~/Music/mp3/ admin@$EPHALENT_IP:/volume1/music/
+rsync -avu -e "ssh -l admin" ~/Documents/ admin@$EPHALENT_IP:/volume1/homes/gaz/documents/
+rsync -avu -e "ssh -l admin" ~/Pictures/Photographs/ admin@$EPHALENT_IP:/volume1/photo/
+rsync -avu -e "ssh -l admin" ~/Library/Application\ Support/1Password/Backups/ admin@$EPHALENT_IP:/volume1/homes/gaz/backups/1password/
 
 # back up documents to dropbox also, excluding peepcode stuff
 rsync -avu --exclude *.mov --exclude *.zip --exclude *.m4v ~/Documents/computer_science/ ~/Dropbox/computer_science/
