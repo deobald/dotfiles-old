e0c68106cc0ee93c89f3a64dd69628cc3d33683a
Gareth Jones <gareth.e.jones@gmail.com>
2011-10-14 21:49:54 -0500

    upgraded to starter-kit v2

---
+++ emacs.d/init.el
@@ -0,0 +1,32 @@
+(require 'package)
+(add-to-list 'package-archives
+              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+   (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(color-theme
+                      color-theme-solarized
+                      starter-kit 
+                      starter-kit-lisp 
+                      starter-kit-bindings
+                      starter-kit-ruby
+                      starter-kit-eshell
+                      go-mode
+                      clojure-mode
+                      clojure-test-mode
+                      clojurescript-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; solarized
+(require 'color-theme-solarized)
+(color-theme-solarized-light)
+
+
+
